e9ea566d9b415e548d1c282b351c1a56543c42da
ulugbekBegmurodov <110164486+ulugbekBegmurodov@users.noreply.github.com>
2023-07-08 08:22:24 +0500

    master commit

---
+++ main2.py
@@ -0,0 +1,63 @@
+
+from web3.middleware import geth_poa_middleware
+from web3 import Web3
+rpc_url = "https://polygon-testnet.public.blastapi.io"
+web3 = Web3(Web3.HTTPProvider(rpc_url))
+
+my_address = '0x8D736A5Bc12908C921BFEc525c5203C7f713Fa9E'
+you_address = '0x4Ba760E5361cf7c9031698ea6dF979a9e989e869'
+
+
+web3.middleware_onion.inject(geth_poa_middleware, layer=0)
+web3.eth.account.enable_unaudited_hdwallet_features()
+
+
+MNEMONIC = 'token devote either act original purse dignity laptop portion suggest rocket call'
+
+account = web3.eth.account.from_mnemonic(MNEMONIC)
+private_key = account._private_key
+
+
+
+def build_txn(
+        *,
+        web3: Web3,
+        from_address: str,
+        to_address: str,
+        amount: float,
+) -> dict[str, int | str]:
+
+    gas_price = web3.eth.gas_price
+
+
+    gas = 2_000_000
+
+    nonce = web3.eth.get_transaction_count(from_address)
+
+    txn = {
+        'chainId': web3.eth.chain_id,
+        'from': from_address,
+        'to': to_address,
+        'value': int(Web3.to_wei(amount, 'ether')),
+        'nonce': nonce,
+        'gasPrice': gas_price,
+        'gas': gas,
+    }
+    return txn
+
+
+transaction = build_txn(
+    web3=web3,
+    from_address=my_address,
+    to_address=you_address,
+    amount=0.333,
+)
+
+
+signed_txn = web3.eth.account.sign_transaction(transaction, private_key)
+
+
+txn_hash = web3.eth.send_raw_transaction(signed_txn.rawTransaction)
+
+
+print(txn_hash.hex())
